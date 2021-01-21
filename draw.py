359e917fd5ac8e07ab2605440c4ca05daeab071f
s1nisteR <ankon1441@gmail.com>
2021-01-21 11:58:15 +0600

    Added draw.py

---
+++ draw.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+import numpy as np
+
+blank = cv.imread('Photos/cat.jpg')
+
+#Creating an image ourselves
+#blank = np.zeros((500,500,3), dtype='uint8') ##np.zeros((height, width, color channnels), dtype='uint8)
+#cv.imshow('Blank Image', blank)
+
+#Manually painting an image over a range
+blank[0:100, 0:100] = 0,255,0
+cv.imshow('Painted Image', blank)
+
+#Drawing a rectangle
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,0,255), thickness=2) #thickness=-1 for fill
+cv.imshow('Rectangle', blank)
+
+#Drawing a circle
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (255,0,0), thickness=-1)
+cv.imshow('Circle', blank)
+
+#Drawing a line
+cv.line(blank, (0,0), (blank.shape[1], blank.shape[0]), (255,255,255), thickness=10)
+cv.imshow('Line', blank)
+
+#Putting text
+cv.putText(blank, "Jesmin", (blank.shape[1]//2, blank.shape[0]//2), cv.FONT_HERSHEY_TRIPLEX, 2.0, (255,255,255), thickness=1)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
+
+
+
+
+
+
+
+
+
+

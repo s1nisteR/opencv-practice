f57e9047044dfbb0ba11a1e4971a2ebe80b193ba
s1nister <ankon1441@gmail.com>
2021-01-25 18:18:14 +0600

    Essential functions added

---
+++ essential.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+
+image = cv.imread('Photos/park.jpg')
+cv.imshow('Original Window', image)
+
+
+
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+cv.imshow('Grayscaled Image', gray)
+
+
+blurredImage = cv.GaussianBlur(image, (7,7), cv.BORDER_DEFAULT) #(25,25) is the kernel size
+                                                                #Can only be positive and odd numbers
+                                                                #Keep both numbers same
+                                                                #Controls the AMOUNT of blurring
+cv.imshow("Blurred Image", blurredImage )
+
+cannyImage = cv.Canny(blurredImage, 100, 175)
+cv.imshow('Original Edge Cascade', cannyImage)
+
+
+dilated = cv.dilate(cannyImage , (7,7), iterations=3)
+cv.imshow('Dilated', dilated)
+
+eroded = cv.erode(dilated, (7,7), iterations=3)
+cv.imshow('Eroded Image', eroded)
+
+
+cv.waitKey(0)
